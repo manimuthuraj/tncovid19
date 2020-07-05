7aa0e193cd0c698dce8567b37db1f7a57437718f
manimuthuraj <66633238+manimuthuraj@users.noreply.github.com>
2020-07-05 15:21:01 +0530

    Update app.js

--- covid/app.js
+++ covid/app.js
@@ -51,4 +51,8 @@ app.get("/state", function(req, res) {
         }
     });
 })
+
+app.get("/about", function(req, res) {
+    res.render("about");
+})
 app.listen(process.env.PORT || 3000, () => console.log(`started`))
